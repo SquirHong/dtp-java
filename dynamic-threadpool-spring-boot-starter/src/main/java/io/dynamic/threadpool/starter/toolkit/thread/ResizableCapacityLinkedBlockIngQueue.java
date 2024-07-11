7317b96fa11d743904f505689e55f14800ec0974
hong <1428981743@qq.com>
2024-07-11 20:29:30 +0800

    fix：解决当增大队列大小时，无法唤醒put阻塞的线程

--- dynamic-threadpool-spring-boot-starter/src/main/java/io/dynamic/threadpool/starter/toolkit/thread/ResizableCapacityLinkedBlockIngQueue.java
+++ dynamic-threadpool-spring-boot-starter/src/main/java/io/dynamic/threadpool/starter/toolkit/thread/ResizableCapacityLinkedBlockIngQueue.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.ReflectUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 可调整大小的阻塞队列
@@ -16,12 +17,20 @@ public class ResizableCapacityLinkedBlockIngQueue<E> extends LinkedBlockingQueue
     }
 
     /**
-     *  修改队列大小
+     * 修改队列大小
      */
     public synchronized boolean setCapacity(Integer capacity) {
         boolean successFlag = true;
         try {
+            int oldCapacity = (int) ReflectUtil.getFieldValue(this, "capacity");
+            AtomicInteger count = (AtomicInteger) ReflectUtil.getFieldValue(this, "count");
+            int size = count.get();
+
             ReflectUtil.setFieldValue(this, "capacity", capacity);
+
+            if (capacity > size && size >= oldCapacity) {
+                ReflectUtil.invoke(this, "signalNotFull");
+            }
         } catch (Exception ex) {
             // ignore
             log.error("动态修改阻塞队列大小失败.", ex);
